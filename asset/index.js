c3949c4ef5d6d4b010f902c927f553668434b85a
RekhaDev <rekhalambotte246@gmail.com>
2021-03-11 10:52:12 +0100

    parcel, Linters and Formatters

---
+++ asset/index.js
@@ -0,0 +1,13 @@
+// Import du display
+import {fet} from "./modules/display.js" ;
+fet()
+
+// import du add
+// import {funcAdd} from "./modules/add.js";
+// funcAdd()
+
+
+//import du delete
+
+
+
---
+++ asset/modules/display.js
@@ -0,0 +1,22 @@
+// Afficher les cards dans la page
+async function fet(){
+    const tpl = document.querySelector("#template-character");
+    const target = document.querySelector("#target");
+    const response= await fetch("https://character-database.becode.xyz/characters")
+    const allCharacter= await response.json()
+    // console.log(allCharacter)
+
+    allCharacter.forEach(({name, shortDescription, description,image}) => {
+        const elt = tpl.cloneNode(true).content;
+
+        elt.querySelector(".card-title").innerText = name;
+        elt.querySelector(".short-description").innerText = shortDescription;
+        elt.querySelector(".description").innerText = description;
+        elt.querySelector("img").src = "data:image/gif;base64,"+image ;
+        target.appendChild(elt);
+    });
+}
+
+// fet() // appel de la fonction d'affichage
+
+export{fet}
